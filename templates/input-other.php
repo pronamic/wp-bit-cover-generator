cd7974d28e5761e6f80244e7ee8118faff2aae4f
Remco Tolsma <info@remcotolsma.nl>
2012-08-21 16:10:52 +0200

    Improved the input fields.

--- templates/input-other.php
+++ templates/input-other.php
@@ -48,45 +48,47 @@ global $bit_cover_generator, $bit_cover;
 	<div class="field">
 		<label for="title"><?php _e( 'Title:', 'bit_cover_generator' ); ?></label>
 		<input id="title" name="title" type="text" maxlength="45" value="<?php echo esc_attr( $bit_cover->title ); ?>" />
-	
-		<div class="color-field">
-			<label for="title-color"><?php _e('Color:', 'bit_cover_generator'); ?></label> 
-			<input id="title-color" name="title-color" type="text" value="<?php echo esc_attr( $bit_cover->title_color ); ?>" />
-		</div>
+	</div>
+
+	<div class="color-field">
+		<label for="title-color"><?php _e( 'Color:', 'bit_cover_generator' ); ?></label> 
+		<input id="title-color" name="title-color" type="text" value="<?php echo esc_attr( $bit_cover->title_color ); ?>" />
 	</div>
 	
 	<div class="field">
-		<label for="eyecatcher-title"><?php _e('Eyecatcher title:', 'bit_cover_generator'); ?></label>
+		<label for="eyecatcher-title"><?php _e( 'Eyecatcher title:', 'bit_cover_generator' ); ?></label>
 		<input id="eyecatcher-title" name="eyecatcher-title" type="text" maxlength="20" value="<?php echo esc_attr( $bit_cover->eyecatcher_title ); ?>" /> 
+	</div>
 	
-		<label for="eyecatcher-text"><?php _e('Eyecatcher text:', 'bit_cover_generator'); ?></label>
+	<div class="field">
+		<label for="eyecatcher-text"><?php _e( 'Eyecatcher text:', 'bit_cover_generator' ); ?></label>
 		<input id="eyecatcher-text" name="eyecatcher-text" type="text" maxlength="60" value="<?php echo esc_attr( $bit_cover->eyecatcher_text ); ?>" />
+	</div>
 	
-		<div class="color-field">
-			<label for="eyecatcher-color"><?php _e('Color:', 'bit_cover_generator'); ?></label>
-			<input id="eyecatcher-color" name="eyecatcher-color" type="text" value="<?php echo esc_attr( $bit_cover->eyecatcher_color ); ?>" />
-		</div>						
+	<div class="color-field">
+		<label for="eyecatcher-color"><?php _e( 'Color:', 'bit_cover_generator' ); ?></label>
+		<input id="eyecatcher-color" name="eyecatcher-color" type="text" value="<?php echo esc_attr( $bit_cover->eyecatcher_color ); ?>" />
 	</div>
 	
 	<div class="field">
-		<label for="footer-title"><?php _e('Footer title:', 'bit_cover_generator'); ?></label>
+		<label for="footer-title"><?php _e( 'Footer title:', 'bit_cover_generator' ); ?></label>
 		<input id="footer-title" name="footer-title" type="text" maxlength="38" value="<?php echo esc_attr( $bit_cover->footer_title ); ?>" /> 
-	
-		<div class="color-field">
-			<label for="footer-title-color"><?php _e('Color:', 'bit_cover_generator'); ?></label>
-			<input id="footer-title-color" name="footer-title-color" type="text" value="<?php echo esc_attr( $bit_cover->footer_title_color ); ?>" />
-		</div>
+	</div>
+
+	<div class="color-field">
+		<label for="footer-title-color"><?php _e( 'Color:', 'bit_cover_generator' ); ?></label>
+		<input id="footer-title-color" name="footer-title-color" type="text" value="<?php echo esc_attr( $bit_cover->footer_title_color ); ?>" />
 	</div>
 	
 	<div class="field">
-		<label for="footer-text"><?php _e('Footer text:', 'bit_cover_generator'); ?></label>
+		<label for="footer-text"><?php _e( 'Footer text:', 'bit_cover_generator' ); ?></label>
 		<input id="footer-text" name="footer-text" type="text" maxlength="200" value="<?php echo esc_attr( $bit_cover->footer_text); ?>" /> 
-	
-		<div class="color-field">
-			<label for="footer-text-color"><?php _e('Color:', 'bit_cover_generator'); ?></label>	
-			<input id="footer-text-color" name="footer-text-color" type="text" value="<?php echo esc_attr( $bit_cover->footer_text_color); ?>" />
-		</div>								
 	</div>
+
+	<div class="color-field">
+		<label for="footer-text-color"><?php _e( 'Color:', 'bit_cover_generator' ); ?></label>	
+		<input id="footer-text-color" name="footer-text-color" type="text" value="<?php echo esc_attr( $bit_cover->footer_text_color); ?>" />
+	</div>		
 	
 	<?php if ( $bit_cover_generator->has_errors() ): ?>
 	
